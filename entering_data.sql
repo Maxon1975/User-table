debd08bed7befe175fa9d54db6365cafb1a6dce5
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2024-10-30 17:28:57 +0300

    Create entering_data.sql

---
+++ entering_data.sql
@@ -0,0 +1,5 @@
+insert into persons (name, surname, age, phone_number,city_of_living)
+values ('Petya', 'Ivanov', 49, 89977993630, 'Moscow'),
+       ('Anna', 'Annenkova', 23, 89099964184, 'Rostov'),
+       ('Natalya', 'Bakeeva', 48, 89777993436, 'Moscow'),
+       ('Oleg', 'Petrov', 25, 89099942345, 'Ufa');
