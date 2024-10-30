96f744a591f1b27235baa577933e5902d670de4a
Maxon1975 <93394865+Maxon1975@users.noreply.github.com>
2024-10-30 17:36:12 +0300

    Update entering_data.sql

--- entering_data.sql
+++ entering_data.sql
@@ -1,4 +1,4 @@
-insert into persons (name, surname, age, phone_number,city_of_living)
+insert into netology.persons (name, surname, age, phone_number,city_of_living)
 values ('Petya', 'Ivanov', 49, 89977993630, 'Moscow'),
        ('Anna', 'Annenkova', 23, 89099964184, 'Rostov'),
        ('Natalya', 'Bakeeva', 48, 89777993436, 'Moscow'),
